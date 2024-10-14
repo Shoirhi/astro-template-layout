74cad0aad29dd2104f9d9af537913573c35efdd6
Shoirhi <men_09_ahem@icloud.com>
2024-10-14 16:16:00 +0900

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
+
+import cloudflare from "@astrojs/cloudflare";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://astro-template-layout.app.dev/", //同じ値をconfig/constantsのENVIRONMENTに設定
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    sitemap(),
+  ],
+  output: "hybrid", // デフォルトでHTMLに事前レンダリング。事前レンダリングをオプトアウトするには、export const prerender = falseを追加。
+  adapter: cloudflare(),
+});
---
+++ src/config.ts
@@ -0,0 +1,19 @@
+import type { Site } from "./types";
+export const IS_PRODUCTION = process.env.NODE_ENV === "production";
+
+export const SITE: Site = {
+  website: IS_PRODUCTION
+    ? "https://astro-template-layout.app.dev/" //同じ値をastro.config.mjsのsiteに設定
+    : "http://localhost:4321/",
+  base: "/",
+  siteName: "Astro Template Layout",
+  titleDefault: "Astro Template Layout",
+  descriptionDefault: "A template of Astro Layout.",
+  ogTypeDefault: "website",
+  ogImageDefault: "/opengraph-image-1200x630.jpg",
+  ogImageTypeDefault: "image/jpeg",
+  ogImageAltDefault: "This is the site image.",
+  lang: "ja",
+  ogLocale: "ja_JP",
+  xTwitterUsername: "@bolstatech",
+};
